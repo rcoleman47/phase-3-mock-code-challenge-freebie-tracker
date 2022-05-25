ff4785642bb83740c833798777636907f705c42d
ryanadams <rcoleman.adams@gmail.com>
2022-05-25 16:15:17 -0500

    commit tree 3bef8535330e0ebe4a55b139f18683f8975d89e3

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,3 @@
 class Freebie < ActiveRecord::Base
-
+  # belongs_to :company, :dev
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,11 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+Freebie.create(item_name: "Stress Ball", value: 4, dev_id: 1, company_id: 1)
+Freebie.create(item_name: "Sheet of paper", value: 2, dev_id: 4, company_id: 3)
+Freebie.create(item_name: "VR Console", value: 350, dev_id: 2, company_id: 2)
+Freebie.create(item_name: "Koozie", value: 12, dev_id: 2, company_id: 4)
+Freebie.create(item_name: "Phone", value: 500, dev_id: 1, company_id: 1)
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
